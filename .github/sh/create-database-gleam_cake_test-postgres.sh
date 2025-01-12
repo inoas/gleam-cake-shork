530d4ae72bb342f5848a8eecd110866485d4bb73
inoas <mail@inoas.com>
2025-01-12 20:14:21 +0100

    fix

--- .github/sh/create-database-gleam_cake_test-postgres.sh
+++ .github/sh/create-database-gleam_cake_test-postgres.sh
@@ -3,8 +3,8 @@
 set -eu
 
 psql <<SQL
-SELECT 'CREATE DATABASE gleam_cake_test'
+SELECT 'CREATE DATABASE gleam_cake_pgo_test'
 WHERE NOT EXISTS (
-  SELECT FROM pg_database WHERE datname = 'gleam_cake_test'
+  SELECT FROM pg_database WHERE datname = 'gleam_cake_pgo_test'
 )\\gexec
 SQL
